e9bb01de5e4ba56dc4d8576dc9a89fe1fb6bb9b3
JeanBenavente <jean.benavente@tecsup.edu.pe>
2025-12-09 02:04:55 -0500

    6

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,11 @@
-require('dotenv').config(); // Esto carga las variables de entorno desde el archivo .env
-
 const { Sequelize } = require('sequelize');
 
-// Usar las variables de entorno para la configuración de la base de datos
-const sequelize = new Sequelize(
-  process.env.DB_NAME,       // Nombre de la base de datos
-  process.env.DB_USER,       // Usuario de la base de datos
-  process.env.DB_PASSWORD,   // Contraseña de la base de datos
-  {
-    host: process.env.DB_HOST, // Host de la base de datos (Railway)
-    port: process.env.DB_PORT, // Puerto de la base de datos
-    dialect: 'mysql',          // Tipo de base de datos
-    logging: false,            // Desactivar logging
-  }
-);
+// Usamos las variables de entorno para configurar la conexión
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql',
+  logging: false, // Puedes desactivar el logging si no lo necesitas
+});
 
 module.exports = sequelize;
